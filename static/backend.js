5048dfb671f9adcae9703ec467c3515214bfbe5f
Ruben Delaet <ruben.delaet@student.howest.be>
2020-03-02 19:02:20 +0100

    Adding record functionality for video capture

--- static/backend.js
+++ static/backend.js
@@ -2,6 +2,10 @@ var buttonVideoStats = document.getElementById("Videostats");
 var buttonBbox = document.getElementById("Bbox");
 var buttonAccuracy = document.getElementById("Accuracy");
 var buttonCrossing = document.getElementById("Crossing");
+var buttonRecord = document.getElementById("record");
+var buttonStop = document.getElementById("stop");
+
+buttonStop.disabled = true;
 
 buttonVideoStats.onclick = function() {
     console.log("Button VideoStats - pressed")
@@ -59,5 +63,48 @@ buttonCrossing.onclick = function() {
     xhr.open("POST", "/toggle");
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.send(JSON.stringify({ toggle: "Crossing" }));
+};
+
+buttonRecord.onclick = function() {
+    // var url = window.location.href + "record_status";
+    buttonRecord.disabled = true;
+    buttonStop.disabled = false;
+
+    // disable download link
+    var downloadLink = document.getElementById("download");
+    downloadLink.text = "";
+    downloadLink.href = "";
+
+    // XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // alert(xhr.responseText);
+        }
+    }
+    xhr.open("POST", "/record_status");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify({ status: "true" }));
+};
+
+buttonStop.onclick = function() {
+    buttonRecord.disabled = false;
+    buttonStop.disabled = true;
+
+    // XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // alert(xhr.responseText);
+
+            // enable download link
+            var downloadLink = document.getElementById("download");
+            downloadLink.text = "Download Video";
+            downloadLink.href = "/static/video.avi";
+        }
+    }
+    xhr.open("POST", "/record_status");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify({ status: "false" }));
 };
 
