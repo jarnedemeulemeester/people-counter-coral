9623e8ca82905b09ae5cd45a31180caea4110ead
Ruben Delaet <ruben.delaet@student.howest.be>
2020-03-04 10:51:18 +0100

    Basic RethinkDB manager class created

--- tools/RethinkDb.py
+++ tools/RethinkDb.py
@@ -1,18 +1,56 @@
 from rethinkdb import r
 from datetime import datetime
 import time
-import json
+import logging
 
-data_dict= {}
+try:
+    logging.basicConfig(filename='tools/Rethinkdb.log', level=logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+except FileNotFoundError:
+    logging.basicConfig(filename='Rethinkdb.log', level=logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-#get insert data
-with r.connect(host='10.10.20.53',port=28015, db='Ruban') as conn:
 
-    for i in range (1,20):
-        time.sleep(2)
-        data_dict["TimeStemp"]=datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        data_dict["PeopleCounted"] = i
-        r.table("Test").insert(data_dict).run(conn)
+class DataManager():
 
-    response = r.table("Test").run(conn)
-    print(response)
+    def __init__(self, host: str, database: str):
+        self._database = database
+        self._host = host
+        try:
+            self.make_connection()
+        except Exception as ex:
+            logging.critical(f"Exception while trying to connect: {ex}")
+
+    def make_connection(self):
+        self._conn = r.connect(host=self._host).repl()
+        logging.info("Succesfully connected to host")
+        if not self._database in r.db_list().run():
+            r.db_create(self._database).run()
+        self._conn.use(self._database).run()
+        logging.info("Succesfully selected database")
+
+    def send_data(self, table, change):
+        data_dict = {}
+        data_dict["TimeStamp"] = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        previous = self._get_latest_value(table)
+        if change == "+1":
+            data_dict["People"] = previous + 1
+        else:
+            data_dict["People"] = previous - 1
+
+        r.table(table).insert()
+
+    def _get_latest_value(self, table):
+        current_count = 0
+        if self._check_table_exist(table):
+            response = r.table("table").run(self._conn)
+            current_count = response[-1]["People"]
+        else:
+            self._make_table()
+        return current_count
+
+    def _check_table_exist(self):
+        raise NotImplemented
+
+    def _make_table(self):
+        raise NotImplemented
---
+++ tools/RethinkDb_test.py
@@ -0,0 +1,4 @@
+from tools.RethinkDb import DataManager
+
+manager = DataManager(host="10.10.20.53", database="Ruban")
+
