1ad467fa03ed7e16ffa10c719e1eac4658c87b23
Ruben Delaet <ruben.delaet@student.howest.be>
2020-03-05 09:14:03 +0100

    Added code clarification

--- tools/RethinkDb.py
+++ tools/RethinkDb.py
@@ -4,24 +4,37 @@ import time
 import logging
 import os
 
-if os.getcwd().split('/')[-1] == "tools":
-    if not os.path.exists("Rethinkdb.log"):
-        with open('Rethinkdb', 'a'):
-            pass
-    logging.basicConfig(filename='Rethinkdb.log', level=logging.INFO,
-                        format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+def create_logging():
+    """
+    This method is in place to write to the corresponding logging file, it checks in what directory it is currently
+    running, and it will resolve if which path it should use to write the logs
+    :return: No return
+    """
+    if os.getcwd().split('/')[-1] == "tools":
+        if not os.path.exists("Rethinkdb.log"):
+            with open('Rethinkdb', 'a'):
+                pass
+        logging.basicConfig(filename='Rethinkdb.log', level=logging.INFO,
+                            format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-else:
-    if not os.path.exists("tools/Rethinkdb.log"):
-        with open('tools/Rethinkdb', 'a'):
-            pass
-    logging.basicConfig(filename='tools/Rethinkdb.log', level=logging.INFO,
-                        format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    else:
+        if not os.path.exists("tools/Rethinkdb.log"):
+            with open('tools/Rethinkdb', 'a'):
+                pass
+        logging.basicConfig(filename='tools/Rethinkdb.log', level=logging.INFO,
+                            format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+create_logging()
 
 
 class DataManager():
 
     def __init__(self, host: str, database: str):
+        """
+        This function is used once for initializing the class, this will try to connect to the database with the given
+        'host' and 'database'.
+        :param host: The address where the database is hosted, this can be an ip or domain name
+        :param database: The name of the specific database you want to connect to
+        """
         self._database = database
         self._host = host
         try:
@@ -30,6 +43,12 @@ class DataManager():
             logging.critical(f"Exception while trying to connect: {ex}")
 
     def make_connection(self):
+        """
+        This function is used to establish a connection with the database, it will connect to the host and check if the
+        chosen database exists. If it does not exist, it will create a new one. After those checks, it will connect to
+        the corresponding database
+        :return: No return
+        """
         self._conn = r.connect(host=self._host, port=28015)
         logging.info("Succesfully connected to host")
         if not self._database in r.db_list().run(self._conn):
@@ -39,6 +58,13 @@ class DataManager():
         logging.info("Succesfully selected database")
 
     def send_data(self, table, action):
+        """
+        This function is used to send data to the database, this will check if the given 'table' exists. If so, it will
+        retreave the latest value and calculate the new one.
+        :param table: The table where your data should be written to
+        :param action: The action that should be taken with the data
+        :return: No return
+        """
         if not self._conn:
             self.make_connection()
         data_dict = {}
@@ -51,9 +77,16 @@ class DataManager():
                 data_dict["People"] = previous - 1
             else:
                 data_dict["People"] = 0
+        logging.info(f"Sending {data_dict} to db.")
         r.table(table).insert(data_dict).run(self._conn)
 
+
     def _get_latest_value(self, table):
+        """
+        This function is only availabe inside the class, it is used to retreave the latest value from a certain table.
+        :param table: The name of the table you want to get the latest value from
+        :return: The latest added value for 'people' in this table
+        """
         if not self._check_table_exist(table):
             self._make_table(table)
         result = r.table(table).order_by(r.desc("TimeStamp")).limit(1).run(self._conn)
@@ -63,9 +96,20 @@ class DataManager():
         return 0
 
     def _check_table_exist(self, table):
+        """
+        This functoin is only available inside the class, it is used to check if the 'table' exists.
+        :param table: The name of the table you want to check
+        :return: True if the table exists and false if it does not
+        """
         logging.info(f"Checking if {table} exists")
         return table in r.table_list().run(self._conn)
 
     def _make_table(self, table):
+        """
+        This function is only available inside the class, it is used to create a new table in the case that you want to
+        write data to a not existing table.
+        :param table: The name the table that will be created
+        :return: No return
+        """
         logging.info(f'Creating table {table}')
         r.table_create(table).run(self._conn)
